e15a112e3dbc1d2c3cc953dcf39614bbe46b771d
Alex Skillin <skillinalex@gmail.com>
2015-08-08 17:25:51 -0500

    Finished defining data structures

--- mhshl.h
+++ mhshl.h
@@ -4,9 +4,14 @@
 #include <vector>
 #include <string>
 
+void spaceBuffer(int len, std::string message);
+void divide();
+
 class ScoringEvent{
 public:
 	ScoringEvent(std::string gID, std::string tID, int gs, int a1, int a2, int per, int sec);
+	std::string toString();
+	std::string printHeading();
 	std::string gameID; // Format for this is MMDDYY-HomeAbbr. For example, 120515-QCB
 	std::string teamID; // The same shorthand team identifier.
 	int scorer;
@@ -19,6 +24,7 @@ public:
 class PenaltyEvent{
 public:
 	PenaltyEvent(std::string gID, std::string tID, int pl, int dur, int per, int sec, std::string charge);
+	std::string toString();
 	std::string gameID;
 	std::string teamID;
 	int player;
@@ -35,6 +41,45 @@ class League{
 public:
 	League();
 	bool varsity;
+	std::vector<Game> games;
+	std::vector<Team> teams;
+	std::vector<Player> players;
+	std::vector<ScoringEvent> goals;
+	std::vector<PenaltyEvent> penalties;
+};
+
+class Team{
+public:
+	Team(std::string abbr, std::string n, std::string c, int identifier);
+	std::string abbreviation;
+	std::string name;
+	std::string city;
+	int id;
+	int gp;
+	int w;
+	int l;
+	int ot;
+	int so;
+	int pts;
+	int gf;
+	int ga;
+	int streak;
+};
+
+class Player{
+public:
+	Player(std::string tID, std::string name, int num);
+	std::string team; // Abbreviation
+	std::string name;
+	int number;
+	int gp;
+	int g;
+	int a;
+	int pp;
+	int sh;
+	int pim;
+	int goalStreak;
+	int pointStreak;
 };
 
 /*
@@ -44,7 +89,8 @@ Game is a basic class to hold information about, well, a game.
 class Game{
 public:
 	Game();
-	
+	std::string toString();
+
 	// Date related stuff
 	int month;
 	int day;
