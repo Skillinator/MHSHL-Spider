c32528b1787edeb50d59d7c07a544e91fdd0e76f
Alex Skillin <16skittles@gmail.com>
2015-09-23 00:18:56 -0400

    Successfully downloads scores now!

--- core.cpp
+++ core.cpp
@@ -58,6 +58,20 @@ std::string getValue(std::string str, std::string value){
 	return str.substr(start, end-start);
 }
 
+int getPeriod(std::string per){
+	if(per == "Period 1")
+		return 1;
+	if(per == "Period 2")
+		return 2;
+	if(per == "Period 3")
+		return 3;
+	if(per == "OT")
+		return 4;
+	if(per == "Shootout")
+		return 5;
+	return 0;
+}
+
 int getMonth(std::string date){
 	std::string month = split(date, " ")[1];
 	int m;
@@ -98,22 +112,124 @@ int getYear(int startYear, int month){
 	return startYear;
 }
 
-void updateGame(Game* g){
+std::string translateTeamID(std::string s){
+
+	if(s == "Ames")
+		return "AMS";
+	if(s == "DM Oak Leafs")
+		return "DMO";
+	if(s == "DM Capitals")
+		return "DMC";
+	if(s == "Sioux City")
+		return "SCM";
+	if(s == "Kansas City")
+		return "KCJ";
+	if(s == "Omaha")
+		return "OJL";
+	if(s == "Quad City")
+		return "QCB";
+	if(s == "Lincoln")
+		return "LJS";
+	if(s == "Dubuque")
+		return "DBQ";
+	if(s == "Mason City")
+		return "MCM";
+	if(s == "Waterloo")
+		return "WAT";
+	if(s == "Cedar Rapids")
+		return "CDR";
+	return "NUL";
+
+}
+
+void processGoal(Game* g, League* l, int p, std::string s){
+	std::string gID="null";
+	std::string tID="null";
+	int scorernum=0;
+	int a1 = 0;
+	int a2 = 0;
+	int per = p;
+	int sec=0;
+	
+	if(s.find("no scoring") != std::string::npos)
+		return;
+
+	std::vector<std::string> firstsplit = split(s, "(");
+	std::vector<std::string> teamscorer = split(firstsplit[0]," - ");
+	/*
+	std::string assists = split(firstsplit[1], ")")[0];
+	std::string time = split(split(firstsplit[1], ")")[1], "<br")[0];
+	*/	
+	std::string team = extract(teamscorer[0], "b");
+	std::string scorer = extract(teamscorer[1], "b");
+
+	gID = g->id;
+	scorernum = std::stoi(getValue(scorer, "playerid"));
+	tID = translateTeamID(team);
+	/*
+	if(assists == "unassisted"){
+		// do nothing
+	}else if(assists.find(", ") == std::string::npos){
+		a1 = std::stoi(getValue(assists, "playerid"));		
+	}else{
+		std::vector<std::string> assistvec = split(assists, ", ");
+		a1 = std::stoi(getValue(assistvec[0], "playerid"));
+		a2 = std::stoi(getValue(assistvec[1], "playerid"));
+	}
+	*/
+	std::cout<<team<<"\n";
+	if(tID == g->homeTeam){
+		std::cout<<"Team " << tID << " == " << g->homeTeam << "\n";
+		g->homeScore++;
+	}else{
+		g->awayScore++;
+	}
+	
+}
+
+void updateGame(Game* g, League* l){
 	std::string url = "midwest-league.stats.pointstreak.com/players-boxscore.html?gameid=" + std::to_string(g->number);
 	std::string page = fetchWebPage(url);
-
-	std::string goals = extract("<table" + split(page, "<table")[5], "table");
-	std::string penalties = extract("<table" + split(page, "<table")[6], "table");
-
-	goals = split(goals, "Scoring Summary")[1];
+	
+	std::string goals = split(page, "Scoring Summary")[1];
+	
 	goals = split(goals, "</table>")[0];
-	goals = "<tr" + split(goals, "<tr")[1];
+
+	std::vector<std::string> goalVec = split(goals, "</td>");
+	goalVec.erase(goalVec.begin());
+	goalVec.erase(goalVec.end());
+
+	
+	for(int i = 0; i < goalVec.size(); i++){
+		goalVec[i] = extract(goalVec[i] + "</td>", "td");
+	}
+	
+	for(int i = 0; i < goalVec.size(); i+=2){
+		if(goalVec[i+1].find("(no scoring)") == std::string::npos ){
+			// Here we do the actual interpretation
+			int per = getPeriod(goalVec[i]);
+			if(goalVec[i+1].find("<tr>") != std::string::npos){
+				std::vector<std::string> firstsplit = split(goalVec[i+1], "<tr>");
+				for(int n = 0; n < firstsplit.size(); n++){
+					if(firstsplit[n].find("<td") != std::string::npos)
+						firstsplit[n] = extract(firstsplit[n], "td");
+					processGoal(g, l, per, firstsplit[n]);
+				}
+			}else{
+				processGoal(g, l, per, goalVec[i+1]);
+			}
+		}
+	}	
 }
 
 void getGames(int season, int team, League *l){
+	
+	std::cout<<"Fetching Games for team " << team << " during the " << season << " season.\n";
+
 	std::string url = "midwest-league.stats.pointstreak.com/players-team-schedule.html?teamid=" + std::to_string(team) + "&seasonid=" + std::to_string(season);
 
 	std::string page = fetchWebPage(url);
+	
 	std::vector<std::string> games = split(extract("<table" + split(page, "<table")[4], "table"), "<tr");
 
 	games.erase(games.begin());
@@ -125,9 +241,11 @@ void getGames(int season, int team, League *l){
 	for(int i = 0; i < games.size(); i++){
 		games[i] = extract("<tr" + games[i], "tr");
 	}
+	
+	std::cout<< "Extracted Games\n";
 
 	for(int i = 1; i < games.size(); i++){
-		
+		std::cout<<"Processing game " << i+1 << " of " << games.size() << "\n";
 		std::string homeID, awayID = " ";
 		int	month, day, year, time, gameID;
 		
@@ -147,11 +265,17 @@ void getGames(int season, int team, League *l){
 		year = getYear(2015, month);
 		time = getMinutes(elements[3]);
 		gameID = stoi(getValue(elements[4], "gameid"));
-	
+		
 		l->addGame(month, day, year, time, homeID, awayID, gameID);
+		
 	}
+	
+}
 
-	// Sort games by date
+void sort_games(League* l){
+	std::cout<<"Sorting Games\n";	
+	// Sort games by date. I probably could move this somewhere else because currently it will repeat for each team. Lot of redundancy.
+
 	bool sorted = false;
 	while(!sorted){
 		sorted = true;
@@ -166,12 +290,18 @@ void getGames(int season, int team, League *l){
 			}
 		}
 	}
+	
+}
 
-	// Update every game, if it exists
+// Procedurally updates every game in the database. Only use when a full rebuild is needed.
+void hard_update(League* l){
 	for(int i = 0; i < l->games.size(); i++){
-		updateGame(&l->games[i]);
+		std::cout<<"Updating game " << i+1 << " of " << l->games.size() << "\n";
+		std::cout<<l->games[i].id<<"\n";
+		updateGame(&l->games[i],l);
 	}
-
+	
+	sort_games(l);
 }
 
 const int SEASON_2014_2015 = 13209;
@@ -183,6 +313,8 @@ int main(){
 	for(int i = 0; i < l.teams.size(); i++){
 		getGames(SEASON_2014_2015, l.teams[i].id, &l);
 	}
+	sort_games(&l);
+	hard_update(&l);
 	showGames(l);
 	std::cout<<l.games.size();
 	return 0;
